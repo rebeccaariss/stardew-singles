7e9b484b4fc580a202d2997a60b0253298596b0b
Rebecca Ariss <rebeccamarionruth@gmail.com>
2022-01-22 13:42:02 -0500

    Fix issue with broken images

--- src/components/UserImages/UserImages.js
+++ src/components/UserImages/UserImages.js
@@ -3,10 +3,15 @@ import React from 'react';
 const UserImages = ({ name }) => {
     return (
         <div>
-            <img className='db center w-30 br2 br--top' src={require(`../../images/${name}/0.png`).default} alt={name}></img>
-            {/* <img src={require(`./images/${name}/1.png`).default} alt={name}></img>
-            <img src={require(`./images/${name}/2.png`).default} alt={name}></img>
-            <img src={require(`./images/${name}/3.png`).default} alt={name}></img> */}
+            <img className='db center w-30 br2 br--top' src={require(`../../images/${name}/0.png`)} alt={name}></img>
+            {/* <img src={require(`../../images/${name}/1.png`)} alt={name}></img>
+            <img src={require(`../../images/${name}/2.png`)} alt={name}></img>
+            <img src={require(`../../images/${name}/3.png`)} alt={name}></img> */}
+            {/* Note that previous formatting for images looked like this:
+
+            <img className='db center w-30 br2 br--top' src={require(`../../images/${name}/0.png`).default} alt={name}></img> 
+            
+            While this previously worked, '.default' now leads to issue with image rendering. */}
         </div>
     )
 }
